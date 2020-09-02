99ccb381312fdfd149a2f8e314e8af535285d82f
Mark Haines <lilserf@gmail.com>
2020-09-01 22:14:58 -0400

    Add db/schema.sql file generated with pg_dump

---
+++ db/schema.sql
@@ -0,0 +1,498 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 12.3
+-- Dumped by pg_dump version 12.3
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+ALTER TABLE ONLY public.player_events DROP CONSTRAINT player_events_game_event_id_fkey;
+ALTER TABLE ONLY public.game_event_base_runners DROP CONSTRAINT game_event_base_runners_game_event_id_fkey;
+ALTER TABLE ONLY public.time_map DROP CONSTRAINT time_map_pkey;
+ALTER TABLE ONLY public.teams DROP CONSTRAINT teams_pkey;
+ALTER TABLE ONLY public.players DROP CONSTRAINT players_pkey;
+ALTER TABLE ONLY public.player_events DROP CONSTRAINT player_events_pkey;
+ALTER TABLE ONLY public.imported_logs DROP CONSTRAINT imported_logs_pkey;
+ALTER TABLE ONLY public.games DROP CONSTRAINT game_pkey;
+ALTER TABLE ONLY public.game_events DROP CONSTRAINT game_events_pkey;
+ALTER TABLE ONLY public.game_event_base_runners DROP CONSTRAINT game_event_base_runners_pkey;
+ALTER TABLE public.teams ALTER COLUMN id DROP DEFAULT;
+ALTER TABLE public.players ALTER COLUMN id DROP DEFAULT;
+ALTER TABLE public.player_events ALTER COLUMN id DROP DEFAULT;
+ALTER TABLE public.imported_logs ALTER COLUMN id DROP DEFAULT;
+ALTER TABLE public.game_events ALTER COLUMN id DROP DEFAULT;
+ALTER TABLE public.game_event_base_runners ALTER COLUMN id DROP DEFAULT;
+DROP TABLE public.time_map;
+DROP SEQUENCE public.teams_id_seq;
+DROP TABLE public.teams;
+DROP SEQUENCE public.players_id_seq;
+DROP TABLE public.players;
+DROP SEQUENCE public.player_events_id_seq;
+DROP TABLE public.player_events;
+DROP SEQUENCE public.imported_logs_id_seq;
+DROP TABLE public.imported_logs;
+DROP TABLE public.games;
+DROP SEQUENCE public.game_events_id_seq;
+DROP TABLE public.game_events;
+DROP SEQUENCE public.game_event_base_runners_id_seq;
+DROP TABLE public.game_event_base_runners;
+DROP PROCEDURE public.wipe_hourly();
+DROP PROCEDURE public.wipe_events();
+DROP PROCEDURE public.wipe_all();
+--
+-- Name: wipe_all(); Type: PROCEDURE; Schema: public; Owner: -
+--
+
+CREATE PROCEDURE public.wipe_all()
+    LANGUAGE plpgsql
+    AS $$begin
+call wipe_events();
+call wipe_hourly();
+end;$$;
+
+
+--
+-- Name: wipe_events(); Type: PROCEDURE; Schema: public; Owner: -
+--
+
+CREATE PROCEDURE public.wipe_events()
+    LANGUAGE plpgsql
+    AS $$begin
+truncate game_events cascade;
+delete from imported_logs where key like 'blaseball-log%';
+truncate time_map;
+end;$$;
+
+
+--
+-- Name: wipe_hourly(); Type: PROCEDURE; Schema: public; Owner: -
+--
+
+CREATE PROCEDURE public.wipe_hourly()
+    LANGUAGE plpgsql
+    AS $$begin
+delete from imported_logs where key like 'compressed-hourly%';
+truncate players cascade;
+truncate teams cascade;
+truncate games cascade;
+end;$$;
+
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: game_event_base_runners; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.game_event_base_runners (
+    id integer NOT NULL,
+    game_event_id integer,
+    runner_id character varying(36),
+    responsible_pitcher_id character varying(36),
+    base_before_play integer,
+    base_after_play integer,
+    was_base_stolen boolean,
+    was_caught_stealing boolean,
+    was_picked_off boolean
+);
+
+
+--
+-- Name: game_event_base_runners_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.game_event_base_runners_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: game_event_base_runners_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.game_event_base_runners_id_seq OWNED BY public.game_event_base_runners.id;
+
+
+--
+-- Name: game_events; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.game_events (
+    id integer NOT NULL,
+    perceived_at timestamp without time zone,
+    game_id character varying(36),
+    event_type text,
+    event_index integer,
+    inning smallint,
+    top_of_inning boolean,
+    outs_before_play smallint,
+    batter_id character varying(36),
+    batter_team_id character varying(36),
+    pitcher_id character varying(36),
+    pitcher_team_id character varying(36),
+    home_score numeric,
+    away_score numeric,
+    home_strike_count smallint,
+    away_strike_count smallint,
+    batter_count integer,
+    pitches character varying(1)[],
+    total_strikes smallint,
+    total_balls smallint,
+    total_fouls smallint,
+    is_leadoff boolean,
+    is_pinch_hit boolean,
+    lineup_position smallint,
+    is_last_event_for_plate_appearance boolean,
+    bases_hit smallint,
+    runs_batted_in smallint,
+    is_sacrifice_hit boolean,
+    is_sacrifice_fly boolean,
+    outs_on_play smallint,
+    is_double_play boolean,
+    is_triple_play boolean,
+    is_wild_pitch boolean,
+    batted_ball_type text,
+    is_bunt boolean,
+    errors_on_play smallint,
+    batter_base_after_play smallint,
+    is_last_game_event boolean,
+    event_text text[],
+    additional_context text,
+    season integer,
+    day integer
+);
+
+
+--
+-- Name: game_events_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.game_events_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: game_events_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.game_events_id_seq OWNED BY public.game_events.id;
+
+
+--
+-- Name: games; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.games (
+    game_id character varying(36) NOT NULL,
+    day integer,
+    season integer,
+    last_game_event integer,
+    home_odds numeric,
+    away_odds numeric,
+    weather integer,
+    series_index integer,
+    series_length integer,
+    is_postseason boolean,
+    home_team character varying(36),
+    away_team character varying(36),
+    home_score integer,
+    away_score integer,
+    number_of_innings integer,
+    ended_on_top_of_inning boolean,
+    ended_in_shame boolean,
+    terminology_id character varying(36),
+    rules_id character varying(36),
+    statsheet_id character varying(36)
+);
+
+
+--
+-- Name: imported_logs; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.imported_logs (
+    id integer NOT NULL,
+    key text,
+    imported_at timestamp without time zone
+);
+
+
+--
+-- Name: imported_logs_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.imported_logs_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: imported_logs_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.imported_logs_id_seq OWNED BY public.imported_logs.id;
+
+
+--
+-- Name: player_events; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.player_events (
+    id integer NOT NULL,
+    game_event_id integer,
+    player_id character varying(36),
+    event_type text
+);
+
+
+--
+-- Name: player_events_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.player_events_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: player_events_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.player_events_id_seq OWNED BY public.player_events.id;
+
+
+--
+-- Name: players; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.players (
+    id integer NOT NULL,
+    player_id character varying(36),
+    valid_until timestamp without time zone,
+    player_name character varying,
+    deceased boolean,
+    hash uuid
+);
+
+
+--
+-- Name: players_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.players_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: players_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.players_id_seq OWNED BY public.players.id;
+
+
+--
+-- Name: teams; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.teams (
+    id integer NOT NULL,
+    team_id character varying(36),
+    location text,
+    nickname text,
+    full_name text,
+    valid_until timestamp without time zone,
+    hash uuid
+);
+
+
+--
+-- Name: teams_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.teams_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: teams_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.teams_id_seq OWNED BY public.teams.id;
+
+
+--
+-- Name: time_map; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.time_map (
+    season integer NOT NULL,
+    day integer NOT NULL,
+    first_time timestamp without time zone
+);
+
+
+--
+-- Name: game_event_base_runners id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.game_event_base_runners ALTER COLUMN id SET DEFAULT nextval('public.game_event_base_runners_id_seq'::regclass);
+
+
+--
+-- Name: game_events id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.game_events ALTER COLUMN id SET DEFAULT nextval('public.game_events_id_seq'::regclass);
+
+
+--
+-- Name: imported_logs id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.imported_logs ALTER COLUMN id SET DEFAULT nextval('public.imported_logs_id_seq'::regclass);
+
+
+--
+-- Name: player_events id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.player_events ALTER COLUMN id SET DEFAULT nextval('public.player_events_id_seq'::regclass);
+
+
+--
+-- Name: players id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.players ALTER COLUMN id SET DEFAULT nextval('public.players_id_seq'::regclass);
+
+
+--
+-- Name: teams id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.teams ALTER COLUMN id SET DEFAULT nextval('public.teams_id_seq'::regclass);
+
+
+--
+-- Name: game_event_base_runners game_event_base_runners_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.game_event_base_runners
+    ADD CONSTRAINT game_event_base_runners_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: game_events game_events_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.game_events
+    ADD CONSTRAINT game_events_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: games game_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.games
+    ADD CONSTRAINT game_pkey PRIMARY KEY (game_id);
+
+
+--
+-- Name: imported_logs imported_logs_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.imported_logs
+    ADD CONSTRAINT imported_logs_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: player_events player_events_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.player_events
+    ADD CONSTRAINT player_events_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: players players_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.players
+    ADD CONSTRAINT players_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: teams teams_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.teams
+    ADD CONSTRAINT teams_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: time_map time_map_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.time_map
+    ADD CONSTRAINT time_map_pkey PRIMARY KEY (season, day);
+
+
+--
+-- Name: game_event_base_runners game_event_base_runners_game_event_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.game_event_base_runners
+    ADD CONSTRAINT game_event_base_runners_game_event_id_fkey FOREIGN KEY (game_event_id) REFERENCES public.game_events(id) ON DELETE CASCADE;
+
+
+--
+-- Name: player_events player_events_game_event_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.player_events
+    ADD CONSTRAINT player_events_game_event_id_fkey FOREIGN KEY (game_event_id) REFERENCES public.game_events(id) ON DELETE CASCADE;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
