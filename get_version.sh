a109ba1cc5ebc3b2156b11c2868caeeeabdb8874
Dillon Lareau <flybye22@gmail.com>
2020-11-21 16:40:00 -0500

    Updating for merge into future-perfect

--- get_version.sh
+++ get_version.sh
@@ -8,7 +8,7 @@ verlt() {
     [ "$1" = "$2" ] && return 1 || verlte $1 $2
 }
 
-docker_version=$(curl -L --fail "https://hub.docker.com/v2/repositories/corvimae/prophesizer/tags/?page_size=1000" | jq '.results | .[] | .name' -r | sed 's/latest//' | sort --version-sort | tail -n 1)
+docker_version=$(curl -L --fail "https://hub.docker.com/v2/repositories/blaseballsibr/prophesizer/tags/?page_size=1000" | jq '.results | .[] | .name' -r | sed 's/latest//' | sort --version-sort | tail -n 1)
 
 cs_version=$(cat prophesizer.csproj | grep "<Version>" | sed 's/ *<\/*Version>//g')
 
