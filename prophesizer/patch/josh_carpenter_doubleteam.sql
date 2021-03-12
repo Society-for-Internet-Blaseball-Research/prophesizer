eba7c3f0cb42cb16c3688df2476223aa5d4739ad
Gizmo <mitchokun@gmail.com>
2021-03-11 20:36:00 -0500

    Patch to fix Josh Carpenter pitching for two teams at once

---
+++ prophesizer/patch/josh_carpenter_doubleteam.sql
@@ -0,0 +1,4 @@
+--Josh Carpenter, playing for both sides
+UPDATE data.team_roster SET valid_until = '2021-03-12 00:01:26' 
+WHERE team_id = '4d921519-410b-41e2-882e-9726a4e54a6a' 
+and valid_from = '2020-11-16 17:22:39.599313' and player_id = '3c331c87-1634-46c4-87ce-e4b9c59e2969';
